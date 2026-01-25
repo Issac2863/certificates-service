08def737df565be5f8933fa9f7d0b21211ac6b77
Joe Defaz <josejoel.defaz@gmail.com>
2026-01-25 16:33:31 -0500

    Fix: resolve merge conflicts

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const logger = new Logger('CertificateServiceBootstrap');
-  const PORT = process.env.PORT || 3003;
+  const PORT = process.env.PORT;
 
-<<<<<<< HEAD
   try {
     const app = await NestFactory.createMicroservice<MicroserviceOptions>(
       AppModule,
@@ -17,15 +16,6 @@ async function bootstrap(): Promise<void> {
           host: '127.0.0.1',
           port: Number(PORT),
         },
-=======
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: '0.0.0.0',
-        port: Number(PORT),
->>>>>>> f407061bfcbe7620020973e075c5e8b36f6d62e9
       },
     );
     
